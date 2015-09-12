9822d868ecb4ef651ef7b47bf7d8075179818a21
neldevfull <nfrugeri@gmail.com>
2015-09-12 20:16:43 +0000

    -- Development form Patient -- 1) Creation form to Patient in HTML and CSS 2) Development form, refactoring for Ruby on Rails 3) Creation routes and methods of the Controllers to form
    -- Previous developments to the first commit --
    1) Creation virtual machine, using Vagrant, 'MedConectaMac'.
    2) Installed development environment, Ruby on Rails.
    3) Development of the initial screen, using HTML and CSS (Bootstrap).
    4) Creation Navbar and Sidebar.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,25 @@
 class PatientsController < ApplicationController
+
+	def new
+		@patient = Patient.new
+	end 
+
+	def create
+		@patient = Patient.new patient_params
+		if @patient.save
+			flash[:notice] = "Paciente cadastrado com sucesso"
+			redirect_to root_path
+		else
+			rendering :new
+		end
+	end
+
+	private
+
+	def patient_params
+		params.require(:patient)
+			.permit :name, :email, :tellephone,
+				:cellphone, :birth, :gender, :mailAccept
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "patients#new"
   get  "patients" => "patients#index"  
+  post "patients" => "patients#create"
 end
