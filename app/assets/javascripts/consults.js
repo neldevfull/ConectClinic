b2bcfe444f4b06cc6d5e2ee0fdaaf65fa0896142
neldevfull <nfrugeri@gmail.com>
2015-09-17 23:19:59 +0000

    -- Improvement in the calendar's style --

--- app/assets/javascripts/consults.js
+++ app/assets/javascripts/consults.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
 	var currentLangCode = 'pt-br';
-	var date = new Date();
-	var d = date.getDate();
-	var m = date.getMonth();
-	var y = date.getFullYear();
-	var name;
-	var start;
-	var end;
+	// var date = new Date();
+	// var d = date.getDate();
+	// var m = date.getMonth();
+	// var y = date.getFullYear();
+
 	var _event;
+	var control = 0;	
 
 	var calendar = $('#calendar').fullCalendar({
 		header: {
@@ -17,38 +16,65 @@ $(document).ready(function() {
 		},
 		defaultView: 'agendaWeek', 
 		lang: currentLangCode,
+		weekends: false,
+		allDaySlot: false,
+		slotDuration: '00:15:00',
+		scrollTime: '08:00:00',
+		timeFormat: 'H:mm',
+		titleFormat: 'DD MMMM YYYY',		
+		businessHours:
+			{
+				start: '8:00',
+	    		end: '18:00'
+	    	},
+	    contentHeight: 460,
 		selectable: true,
 		selectHelper: true,		
-		editable: true,				
-		select: function(start, end) {	
-			$('#name').val('');							
+		editable: true,	
+		// Insert consultation			
+		select: function(start, end) {		
+			$('#name').val('');								
 			$('#dialog').dialog("open");
 			_event = {
 				start: start,
 				end: end 
 			};					
 			calendar.fullCalendar('unselect');
-		}, 	
+			control = 1;
+		}, 
+		// Update consultation
+		eventClick: function(event, element) {
+			$('#name').val(event.title);
+			$('#dialog').dialog("open");
+	        _event = event;	        
+	        control = 2;
+	    }	
 	});
 
 	$("#dialog").dialog({
         autoOpen: false,
         height: 350,
         width: 700,
-        modal: true,
+        modal: false,
         buttons: {
-            'Create event': function () { 
+            'Agendar': function() { 
             	_event.title = $('#name').val();
-            	$('#calendar').fullCalendar('renderEvent', _event, true);  	            	
+            	if(control == 1)
+            		$('#calendar').fullCalendar('renderEvent', _event, true);  	            	
+            	else if(control == 2)
+            		$('#calendar').fullCalendar('updateEvent', _event);                
+            	$(this).dialog('close');
+            },
+            'Remover': function() {
+                $('#calendar').fullCalendar('removeEvents', _event._id);
                 $(this).dialog('close');
             },
-            Cancel: function () {
-                $(this).dialog('close');
-            }
+            'Fechar': function() {            	
+            	$(this).dialog('close');
+            }            
         },
 
         close: function () {
         }
-    });   
-
+    });  
 });
