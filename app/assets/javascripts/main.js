df0527f5e6686dfc5d48d9ce1c992b3fbbf7a22c
neldevfull <nfrugeri@gmail.com>
2015-11-03 18:51:36 +0000

    Start Development NewDesign

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,10 +2,17 @@
 	"use strict";
 
 	$(function() {
+		//  Get domain
 		var domain = window.location.href.toString()
 			.split(window.location.host + '/')[1];		
+		
+		// Load modules
 		var resource = domain.split('/')[0];
 		var module   = modulejs.require(resource);		
+		var events   = modulejs.require('events');
+		
+		// Execute modules
 		module();
+		events();
 	});
 })()
---
+++ app/assets/javascripts/modules/interfaces/events.js
@@ -0,0 +1,9 @@
+modulejs.define('events', function() {
+	return function() {		
+		// $('.menu a').click(function(){
+		// 	$('.menu a').css({
+		// 		'background-color': 'red'
+		// 	});
+		// });		
+	}
+});
